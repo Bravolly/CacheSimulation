c1958ce562fc95cd2046445c4090bd37a51cb531
Bravolly <pbravolly@yahoo.com>
2017-05-01 14:42:42 -0700

    convert struct trace to a pointer and added comments

--- main.c
+++ main.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * Max size of one line in the trace
- */
-#define LINELENGTH 40
+#define LINELENGTH 40   // Max size of one line in the trace
+#define BLOCK 16        // Block size for cache in bytes
 
-/*
- *
- */
-struct Trace_ {
-    char *instr;//[9]
-    char type;
-    char *dataAcc;//[10];
-    char byteScanned;
-    char *dataDisp;//[10];
-};
+/* Structure of a trace line */
+typedef struct {
+    char *instr;        // [9] address of instruction
+    char type;          // read/write flag
+    char *dataAcc;      // [10] address of data access
+    char byteScanned;   // number of bytes read/written
+    char *dataDisp;     // [10] data read/written
+} Trace_;
 
 int main(int argc, char **argv) {
     int i,j;
@@ -23,11 +19,16 @@ int main(int argc, char **argv) {
     FILE *file;
     char *buffer = malloc(sizeof(char) * LINELENGTH);
 
-    /*
-     * create trace storage
-     */
-    struct Trace_ trace;
-    trace.instr = malloc(sizeof(char) * 9);
+    /* Error check for correct number of arguments */
+    if (argc > 3) {
+        fprintf(stderr, "Error: Too many arguments needed\n"
+                "Example: ./producer-consumer (producer) (consumer) (number of items)");
+        exit(1);
+    }
+
+    /* Create trace storage */
+    Trace_ *trace = malloc(sizeof(Trace_));
+    trace->instr = malloc(sizeof(char) * 9);
 
     printf("Cache Simulator - System 1\n");
 
@@ -42,15 +43,18 @@ int main(int argc, char **argv) {
         i = 0;
         j = 0;
 
+        /* store instruction address */
         while (buffer[i] != ':') {
-            trace.instr[j] = buffer[i];
+            trace->instr[j] = buffer[i];
             i++;
             j++;
         }
 
+        /* store read/write flag */
         i += 2;
-        trace.type = buffer[i];
+        trace->type = buffer[i];
 
+        /* determine length of address from trace for malloc*/
         i += 2;
         sizeOffset = 0;
         while (buffer[i] == ' ') {
@@ -58,17 +62,20 @@ int main(int argc, char **argv) {
             sizeOffset++;
         }
 
+        /* store address of data access */
+        trace->dataAcc = malloc(sizeof(char) * (10 - sizeOffset));
         j = 0;
-        trace.dataAcc = malloc(sizeof(char) * (10 - sizeOffset));
         while (buffer[i] != ' ') {
-            trace.dataAcc[j] = buffer[i];
+            trace->dataAcc[j] = buffer[i];
             i++;
             j++;
         }
 
+        /* store number of bytes read/written */
         i += 2;
-        trace.byteScanned = buffer[i];
+        trace->byteScanned = buffer[i];
 
+        /* determine length of address from trace for malloc*/
         i += 2;
         sizeOffset = 0;
         while (buffer[i] == ' ') {
@@ -76,16 +83,16 @@ int main(int argc, char **argv) {
             sizeOffset++;
         }
 
-
-        trace.dataDisp = malloc(sizeof(char) * (10 - sizeOffset));
+        /* store data read/written */
+        trace->dataDisp = malloc(sizeof(char) * (10 - sizeOffset));
         j = 0;
         while (buffer[i] != '\n') {
-            trace.dataDisp[j] = buffer[i];
+            trace->dataDisp[j] = buffer[i];
             i++;
             j++;
         }
 
-        printf("%s %c %s %c %s\n", trace.instr, trace.type, trace.dataAcc, trace.byteScanned, trace.dataDisp);
+        printf("%s %c %s %c %s\n", trace->instr, trace->type, trace->dataAcc, trace->byteScanned, trace->dataDisp);
     }
 
     return 0;
